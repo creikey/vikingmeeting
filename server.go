3f75b2d34935539fabd30af615319c8ab07092bb
Cameron Reikes <cameronreikes@gmail.com>
2018-01-27 22:46:12 -0800

    Remove useless chat module

--- server.go
+++ server.go
@@ -10,18 +10,6 @@ const (
 
 func main() {
 	log.Println("Starting server")
-	test := NewChat("Hello!")
-	log.Printf("Chat to write|%v|filename of the log: %v", test, LOGFILE)
-	err := test.LogToFile(LOGFILE)
-	if err != nil {
-		log.Fatal(err)
-	}
-	/*logf, err := os.Open(LOGFILE)
-	if err != nil {
-		log.Fatal(err)
-	}
-	logf.WriteString("Test")
-	logf.Close()*/
 }
 
 /*func mainHandler(w http.ResponseWriter, r *http.Request) {
