023343a1c178751ae3f68ba7171ee797edd27b26
Cameron Reikes <cameronreikes@gmail.com>
2018-01-28 00:00:42 -0800

    Add basic http server

--- server.go
+++ server.go
@@ -1,18 +1,43 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
+	"net/http"
 )
 
 const (
 	LOGFILE = "log.txt"
 )
 
-func main() {
-	log.Println("Starting server")
+func getStyles(w http.ResponseWriter, r *http.Request) {
+	log.Println("Request for styles.css")
+	page, err := ioutil.ReadFile("css/styles.css")
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write(page)
 }
 
-/*func mainHandler(w http.ResponseWriter, r *http.Request) {
-	title := "Viking Games Meeting"
-	p, _ := loadPa
-}*/
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	// fmt.Fprintf(w, "H("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("resources")))) ello there! I love %s!", r.URL.Path)
+	log.Println("Request for index.html")
+	page, err := ioutil.ReadFile("html/index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write(page)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Redirecting reqest for " + r.URL.Path + " to index.html")
+	http.Redirect(w, r, "/index.html", http.StatusFound)
+}
+
+func main() {
+	log.Println("Starting server")
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/index.html", mainHandler)
+	http.Handle("/css/", http.StripPrefix("/css", http.FileServer(http.Dir("css"))))
+	http.ListenAndServe(":8080", nil)
+}
